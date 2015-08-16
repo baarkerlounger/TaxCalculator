49f009428887a872ff78ebe1bc3296d0d2665cff
db579 <baarkerlounger@gmail.com>
2015-08-16 21:03:55 +0800

    12.08.15

--- app/src/main/java/baarkerlounger/com/costabroad/UKTaxCalculator.java
+++ app/src/main/java/baarkerlounger/com/costabroad/UKTaxCalculator.java
@@ -3,19 +3,21 @@ package baarkerlounger.com.costabroad;
 import java.math.BigDecimal;
 
 /**
- * Created by dan on 8/10/15
+ * Class to calculate Gross and Net income based on UK income tax system
  */
 class UKTaxCalculator {
 
+    final private static BigDecimal INITIAL_TAX_FREE_THRESHOLD = BigDecimal.valueOf(10600);
     final private static BigDecimal HIGHER_RATE_THRESHOLD = BigDecimal.valueOf(31865);
     final private static BigDecimal ADDITIONAL_RATE_THRESHOLD = BigDecimal.valueOf(150000);
     final private static BigDecimal BASIC_RATE = BigDecimal.valueOf(0.2);
     final private static BigDecimal HIGHER_RATE = BigDecimal.valueOf(0.4);
     final private static BigDecimal ADDITIONAL_RATE = BigDecimal.valueOf(0.45);
+    final private static BigDecimal TAX_FREE_REDUCTION_THRESHOLD = BigDecimal.valueOf(100000);
 
     BigDecimal getUKGross(BigDecimal net){
 
-        BigDecimal TAX_FREE_THRESHOLD = BigDecimal.valueOf(10000);
+        BigDecimal TAX_FREE_THRESHOLD = INITIAL_TAX_FREE_THRESHOLD;
 
         if(net.compareTo(TAX_FREE_THRESHOLD) <= 0){
             return net;
@@ -27,10 +29,11 @@ class UKTaxCalculator {
 
         BigDecimal net;
         BigDecimal tax;
-        BigDecimal TAX_FREE_THRESHOLD = BigDecimal.valueOf(10000);
+        BigDecimal TAX_FREE_THRESHOLD = INITIAL_TAX_FREE_THRESHOLD;
 
-        if (gross.compareTo(BigDecimal.valueOf(100000)) > 0){
-            BigDecimal amountOverReductionThreshold = gross.subtract(BigDecimal.valueOf(100000));
+        //For every GBP2 over 100,000 earned, Tax Free Allowance reduces by GBP1
+        if (gross.compareTo(TAX_FREE_REDUCTION_THRESHOLD) > 0){
+            BigDecimal amountOverReductionThreshold = gross.subtract(TAX_FREE_REDUCTION_THRESHOLD);
             if (amountOverReductionThreshold.compareTo(TAX_FREE_THRESHOLD) > 0) {
                 TAX_FREE_THRESHOLD = TAX_FREE_THRESHOLD.subtract(amountOverReductionThreshold);
             }
